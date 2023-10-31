8abbb05e131f68f66c848748ef269c98e15c1936
magesh magi <95517003+mageshmagi44@users.noreply.github.com>
2023-10-31 17:25:21 +0530

    Update Untitled-1.py
    created prepend in linked list

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,2 +1,12 @@
-class mag:
-    pass
+class stack:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class linked_list(self):
+    def __init__(self):
+        self.head=None
+
+    def prepend(self,val):
+        newnode=node(val)
+        newnode.next=self.head
+        self.head=newnode
